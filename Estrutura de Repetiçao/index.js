55bed3e961490a4c03a58b3cce066f35e826de6b
Daniel Almeida <daniellsilvaoficial@gmail.com>
2022-01-11 20:04:31 -0300

    Atualização do dia 11/01/22

---
+++ Estrutura de Repetiçao/index.js
@@ -0,0 +1,33 @@
+let cont = 0;
+let nome = "Olá Mundo";
+
+do {
+
+    console.log(nome[cont]);
+    cont++;
+
+} while(cont < nome.length);
+
+//-----------------------------------------------------------------------------------------
+
+let conte = 0
+
+while(conte < 5 ){
+
+    console.log('Parabens!!!');
+    conte++;
+}
+
+//-------------------------------------------------------------------------------------------
+
+for (let i = 1 ; i < 6; i++){
+    console.log(i);
+}
+
+let i = 1
+for ( ; ; ){
+    if (i < 6)
+    console.log(i);
+    i++;
+}
+
